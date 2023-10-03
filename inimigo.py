cc8ed201bd4b458e4e2de1dff7770ed757fe6203
gutodidonato <rm99433@fiap.com.br>
2023-10-03 14:09:27 -0300

    inimigo dificuldade reduzida

--- inimigo.py
+++ inimigo.py
@@ -1,15 +1,17 @@
 from jogador import *
+import random as r
 
 VELOCIDADE = 20
 POSICAO_X = 350
 COMPUTADOR_POSICAO_INICIAL = (POSICAO_X, 0)
+DIFICULDADE = 1
 
 
 class Computador(Jogador):
     
     def __init__(self):
         super().__init__()
-        self.dificuldade = 1
+        self.dificuldade = (r.randint(1,20)+ DIFICULDADE)
         self.posicaoAtual = 0
         self.posicaoX = POSICAO_X
         self.criar_jogador(COMPUTADOR_POSICAO_INICIAL)
