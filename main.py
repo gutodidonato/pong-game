e11feaf19c8aa2dfdd86a536ac8a40fc0665fbb4
gutodidonato <rm99433@fiap.com.br>
2023-10-02 23:40:52 -0300

    colisão com jogador ok

--- main.py
+++ main.py
@@ -2,36 +2,43 @@ from turtle import *
 from jogador import *
 from bola import *
 
+def quicar_parede():
+    tela.tracer(0)
+    estado_novo = 360 - bola.estado_de_lance
+    bola.corecao_lance(estado_novo)
+    bola.mover()
+    tela.update()
+    tela.tracer(1)
+    
+        
+def quicar_jogador(jogador):
+    (posx, posy) = bola.pos()
+    for parte in jogador.corpo:
+        print(parte.pos())
+        if parte.distance(posx, posy) < 20:
+            quicar_parede()
+    
+    
+    
+    
 tela = Screen()
 tela.screensize(bg="black")
 tela.setup(width=800, height=600)
 tela.title("Pingue-Pongue")
 
 jogador = Jogador()
-
-
 jogador.criar_jogador()
 
-
-def quicar_parede():
-    if bola.ycor() > 280 or bola.ycor() < -280:
-        tela.tracer(0)
-        estado_novo = 360 - bola.estado_de_lance
-        bola.corecao_lance(estado_novo)
-        bola.mover()
-        tela.update()
-        tela.tracer(1)
-    
-
 bola = Bola()
+
 while True:
     bola.mover()
-    print(bola.ycor())
-    quicar_parede()
+    if bola.ycor() > 280 or bola.ycor() < -280:
+        quicar_parede()
+    quicar_jogador(jogador)
+
+
     
-
-
-
 
 
 
